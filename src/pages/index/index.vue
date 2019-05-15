a1c7d05ce59e61fae7052b4a33475febd3238972
libei0625 <22060821987@qq.com>
2019-05-15 10:22:58 +0800

    错误

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -131,20 +131,6 @@ export default {
   },
 
   methods: {
-    bindViewTap () {
-      const url = '../logs/main'
-      if (mpvuePlatform === 'wx') {
-        mpvue.switchTab({ url })
-      } else {
-        mpvue.navigateTo({ url })
-      }
-    },
-    clickHandle (ev) {
-      console.log('clickHandle:', ev)
-      // throw {message: 'custom test'}
-    }
-  },
-
   created () {
     // let app = getApp()
   }
